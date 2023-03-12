fc7087d98d6d170e9b95cb212e7684f4325cd999
Слава <this-is-yashka@yandex.ru>
2023-03-12 15:13:20 +0500

    model_testing done

--- model_testing.py
+++ model_testing.py
@@ -1,1 +1,9 @@
-#создайте python-скрипт , проверяющий модель машинного обучения на построенных данных из папки “test”.
+import pandas as pd
+import pickle
+from sklearn.metrics import f1_score
+LogReg = pickle.load(model)
+X_test = pd.read_csv('test/X_val.csv', delimiter = ',')
+Y_test = pd.read_csv('test/Y_val.csv', delimiter = ',')
+y_preds = LogReg.predict(X_test)
+f1_score(Y_test, y_preds, average='micro')
+
