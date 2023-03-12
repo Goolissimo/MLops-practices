6a2402d8ff098b649790b63b6e575c55a4c333ac
Alexadr45 <115409750+Alexadr45@users.noreply.github.com>
2023-03-12 15:07:00 +0500

    Update model_preprocessing.py

--- model_preprocessing.py
+++ model_preprocessing.py
@@ -3,9 +3,11 @@ import pandas as pd
 
 df_train = pd.read_csv('train/X_train.csv', index_col=0)
 df_test = pd.read_csv('test/X_val.csv', index_col=0)
+#Объединяем датафреймы
 df_total = pd.concat([df_train, df_test])
 
 
+#Делим столбцы на категориальные и числовые
 cat_columns = []
 num_columns = []
 for column_name in df_total.columns:
@@ -15,14 +17,17 @@ for column_name in df_total.columns:
         num_columns +=[column_name]
 
 
+#Применение One-hot encoding
 ohe_cat_col = pd.get_dummies(df_total[cat_columns])
 df_total = df_total.join(ohe_cat_col)
 df_total.drop(columns=['id', 'code', 'period'], inplace=True)
 
 
+#Разделение обратно на тренировочную и тестовую выборки
 X_train = df_total.iloc[0:df_train.shape[0],:]
 X_test = df_total.iloc[df_train.shape[0]:,:]
 
 
+#Сохранение файлов
 X_train.to_csv('/content/X_train.csv', index=False)
 X_test.to_csv('/content/X_test.csv', index=False)
