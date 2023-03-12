b84c4b41c6f2e2b51f33b29d265a7f48eca45839
Савелий <113251478+Goolissimo@users.noreply.github.com>
2023-03-12 14:03:32 +0500

    Create pipeline.sh

---
+++ pipeline.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+#создание директорий data, в которую будет загружен датасет, папок train и test, в которых будут разделенные данные
+mkdir data, train, test
+
+
+#устанавливаем необходимые библиотеки
+pip install gdown
+pip install scikit-learn
+pip install pandas
+
+
+#последовательно запускаем файлы, согласно заданию
+python3 data_creation.py #загружает файл с гугл диска и делит данные на train и test
+python3 data_preprocessing.py #предобработка данных
+python3 model_preparation.py #обучение модели
+python3 model_testing.py #проверка модели на тестовых данных, получение метрики
