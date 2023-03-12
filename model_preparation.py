13ac20dbd8e7c38b0bca833aaa6f341e57bdd25c
antosha <antosha@Ubuntu.myguest.virtualbox.org>
2023-03-12 14:38:51 +0500

    train model scrypt

--- model_preparation.py
+++ model_preparation.py
@@ -1,1 +1,14 @@
 #создайте python-скрипт , который создает и обучает модель машинного обучения на построенных данных из папки “train”.
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
+import pickle
+
+X_train = pd.read_csv('train/X_train.csv', delimiter = ',')
+y_train = pd.read_csv('train/Y_train.csv', delimiter = ',')
+model = LogisticRegression(fit_intercept=True,
+                            penalty='l1',solver='liblinear',
+                            C=8.75,
+                            max_iter=10000)
+model.fit(X_train, y_train)
+
+pickle.dump(model)
