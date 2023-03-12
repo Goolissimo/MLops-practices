e3da694a8f6ab1fc9fe08c2bb6ab5ef35979d6e3
Савелий <113251478+Goolissimo@users.noreply.github.com>
2023-03-12 15:59:10 +0500

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -13,6 +13,6 @@ pip install pandas
 
 #последовательно запускаем файлы, согласно заданию
 python3 data_creation.py #загружает файл с гугл диска и делит данные на train и test
-python3 data_preprocessing.py #предобработка данных
+python3 model_preprocessing.py #предобработка данных
 python3 model_preparation.py #обучение модели
 python3 model_testing.py #проверка модели на тестовых данных, получение метрики
