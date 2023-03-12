a32e04dd87b2b7f2df4617018855b3dffe35db1c
Савелий <113251478+Goolissimo@users.noreply.github.com>
2023-03-12 15:53:05 +0500

    Update data_creation.py

--- data_creation.py
+++ data_creation.py
@@ -4,7 +4,7 @@ import gdown
 
 
 #скачиваем csv файл с гугл диска и сохраняем в папке data
-gdown.download(id="1waefPsrT7sm5rsRMjDHGCXreY45q9tcY", output="/data/dataset.csv", quiet=False)
+gdown.download(id="1waefPsrT7sm5rsRMjDHGCXreY45q9tcY", output="./data/dataset.csv", quiet=False)
 #открываем данные в виде датафрейма
 df = pd.read_csv('data/dataset.csv', delimiter = ',', index_col = 0)
 #делим данные на тренировочные и тестовые
